d8c8693255c2d34e5326b45880a7bad298dfec83
ELFletch04 <64966787+ELFletch04@users.noreply.github.com>
2024-08-19 21:23:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,246 @@
+const products = {
+    weed: [
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_0049f_Thsyh8o3JWessCNCR5a4iHzz/f800/1724085814.jpeg?s=322711ad4677d3a744506ea293656ae48adf7246',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909167741-weed-croc-charms'
+            
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_00697_rffs8n7dx6d6b7Bn4ojLU9Kt/f800/1724085781.jpeg?s=d7913239ccd4262b596a18b5f5b219c2afd939fe',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909164381-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_01a19_TE4H7RMreZ9iY8EUTSa1eq4S/f800/1724085750.jpeg?s=10f7ae5b849b17fd81def6305a442ab4edf62445',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909161133-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_026e5_Ar95Fi7BEm1GMwWcGw38odsJ/f800/1724085704.jpeg?s=44a9a85efc1f546c3d117e4b889e202675045671',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909156299-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_02295_8u6HqcCPnugDuw36KHr2NFa3/f800/1724085667.jpeg?s=c0566d401bcb5aa8a8c4140c95889381d84bcb83',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909152535-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_00ef3_xo9bs2oCZXzuuXNhS9j88c6m/f800/1724085631.jpeg?s=fa694487add1aed2693ecc2ca0d92c5a6a4e53be',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909148828-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_002b2_D4cPc7miLGMLbs2ofvomW59w/f800/1724085584.jpeg?s=3adeab5583ba5aa4dc8f73f35cb8d97caec76e43',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909143902-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_008e9_siHoD6aaviQPggG7PZk471Go/f800/1724085539.jpeg?s=caf43c70e20e0bb3d5c31ae8a2494e7d1fe79551',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909139396-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_00ba3_AydVJfMBHakNgUmPXcicem13/f800/1724085427.jpeg?s=64329a2a312f23e86bb57131112ccefe72b83281',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909127621-weed-croc-charms'
+        },
+        {
+            name: 'Weed Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_00f26_xQacv4bCppmuBK2NitKYD53h/f800/1724085467.jpeg?s=24565e405364f97741c4bb3f8a19076b7b274c9e',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909131687-weed-croc-charms'
+        },
+        // Add other weed products here
+    ],
+    cars: [
+        {
+            name: 'Cars Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_00c23_3i687o9s6Z6FK26tQDPccZ8h/f800/1724085325.jpeg?s=f22bf840ebe40f3631db74fe711d56a4888a7d46',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909117011-cars-croc-charms',
+            
+            
+            
+        },
+        {
+            name: 'Cars Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_02420_Gs74UDBSwXQPdVDpnadFGSrQ/f800/1724085272.jpeg?s=f9081f3eb41f797e3120b256b7a9d709c64b49f7',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909111461-cars-croc-charms'
+        },
+        {
+            name: 'Cars Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_0253b_E4HLL34eRQ47MCZNbmbjTevV/f800/1724085220.jpeg?s=a349e2dfb50c3bb6a65d58d40834973ddde0ea90',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909105978-cars-croc-charms'
+        },
+        {
+            name: 'Cars Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_01ae6_id9YjXCxJRgtUAttftmu28yy/f800/1724085175.jpeg?s=b9fb59103852a333dcb17b6c2a582c7142398004',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909101435-cars-croc-charms'
+        },
+        {
+            name: 'Cars Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_0239c_r6TmiKPY8JnvbszAeMNTeaGK/f800/1724085086.jpeg?s=7917c0e836b0ffa8f012b8c586b69a525efaca23',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909091948-cars-croc-charms'
+        },
+        // Add other cars products here
+    ],
+    toystory: [
+        {
+            name: 'Buzz Lightyear',
+            imageUrl: 'https://images1.vinted.net/t/01_023ae_Ztss2m4GMca1Jhx2qhPRiYKj/f800/1724084476.jpeg?s=d94bb9686fb8d41a9cff4e465db237329885f33f',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909026981-toy-story-croc-charms'
+        },
+        {
+            name: 'Buzz Lightyear',
+            imageUrl: 'https://images1.vinted.net/t/01_000d3_7mgAXFsT7TPnrZhQnij9w3eR/f800/1724084389.jpeg?s=28d713376fa71d29d72cea691613acfe8e429e6f',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909017627-toy-story-croc-charms'
+        },
+        {
+            name: 'Woody and Jessie',
+            imageUrl: 'https://images1.vinted.net/t/03_017a4_gMC8xRxoR7E92Rj26K5v8mMf/f800/1724084325.jpeg?s=2e9ceb6b034a793be2603b6472fad5600ba92573',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909010805-toy-story-croc-charms'
+        },
+        {
+            name: 'Woody and Jessie',
+            imageUrl: 'https://images1.vinted.net/t/02_001d9_aXabvAaqAE2pMJQ8LGanbafM/f800/1724084283.jpeg?s=433af4ea2228581f79c2261f2eabb7cda7dc2021',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909006396-toy-story-croc-charms'
+        },
+        {
+            name: 'Aliens',
+            imageUrl: 'https://images1.vinted.net/t/01_00a23_T7vx9yCxYbQ2oLeBjXpiLiXp/f800/1724084154.jpeg?s=5ed9e8365476ac3a30a5891c5c7f55b4d57373a8',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908992528-toy-story-croc-charms'
+        },
+        {
+            name: 'Mr and Mrs Potato Head',
+            imageUrl: 'https://images1.vinted.net/t/01_02673_P6ZtWagig5NE6W4SbkexBpXu/f800/1724084115.jpeg?s=90e6032bf9d342e4a53a97eb4b66665e2069d665',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908988393-toy-story-croc-charms'
+        },
+        // Add other Toy Story products here
+    ],
+    insideout: [
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_02228_v3dgUdSQMgH5oDkPg9uNpSeX/f800/1724084940.jpeg?s=4f62847647bb465fbd26af75f638b1d2330ac711',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909076454-inside-out-2-croc-charms'
+        },
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_01efe_fH2wLPJ9CEfTVEv9mkVhECgM/f800/1724084846.jpeg?s=abbe50ec910ac3a589aefc34179b004a58185a00',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909066496-inside-out-2-croc-charms'
+        },
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_00240_RwbN8bDVAm31HDL8ys7G8kQv/f800/1724084796.jpeg?s=5cf99818527087492cc95e6e0324facc487cbb54',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909061154-inside-out-2-croc-charms'
+        },
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_00917_9dkKjWvVfmocna5Pn3JSM1PB/f800/1724084742.jpeg?s=7bab9edd383673ffb1c0238772cccbdc6b84ae62',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909055430-inside-out-2-croc-charms'
+        },
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_0019f_8xFaBw6MnXUhf3SKt3d8wpDw/f800/1724084703.jpeg?s=df5e260afa5fc43bf43606560bf0a67c0aae5f1b',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909051274-inside-out-2-croc-charms'
+        },
+        {
+            name: 'Inside Out Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_01e65_sU4x6kqAdZUssubqApq1xvm5/f800/1724084646.jpeg?s=26c83d382c8f66313604bfdaa91ed8d185d260b3',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4909045188-inside-out-2-croc-charms'
+        },
+        // Add other Inside Out products here
+    ],
+    spiderman: [
+        {
+            name: 'Spiderman Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_012de_btxnkYUyyPGeXUxLzZ3qCU1h/f800/1724084046.jpeg?s=daeeb0cf03623b3c81a19316ffeebe4fbefb1bdb',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908981006-spider-man-croc-charms'
+        },
+        {
+            name: 'Spiderman Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_02269_z4QG78n3xtuwPQDe1cJA9RDo/f800/1724084003.jpeg?s=a7b0634a39dddc5ca2073408488e672e905193bc',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908976349-spider-man-croc-charms'
+        },
+        {
+            name: 'Spiderman Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/01_005da_ibLuG72NrKP9obVA5LbRT7KZ/f800/1724083940.jpeg?s=84c1f0235c4da2e4008316766e60412d4e3f7540',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908969555-spider-man-croc-charms'
+        },
+        {
+            name: 'Spiderman Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/02_008e0_YbJphgbfKcLaRKjTR5wes3u6/f800/1724083898.jpeg?s=4df42e6d02d81e12783fd0660f695c5f18774fd8',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908964952-spider-man-croc-charms'
+        },
+        {
+            name: 'Spiderman Croc charms',
+            imageUrl: 'https://images1.vinted.net/t/03_01530_oNn9oqyo9uRrnPdrYffQg45o/f800/1724083819.jpeg?s=41a903efb5925cf880c875c469cd4900a3090eae',
+            price: 1.00,
+            link: 'https://www.vinted.co.uk/items/4908956640-spider-man-croc-charms'
+        },
+        // Add other Spiderman products here
+    ]
+};
+
+function renderProducts(category) {
+    const productGrid = document.querySelector('.product-grid');
+    productGrid.innerHTML = '';
+
+    const categoryProducts = products[category] || [];
+
+    categoryProducts.forEach(product => {
+        const markup = `
+            <div class="product-card">
+                <a href="${product.link}" target="_blank">
+                    <img src="${product.imageUrl}" alt="${product.name}">
+                    <h2>${product.name}</h2>
+                    <p>£${(product.price ).toFixed(2)}</p>
+                    
+                </a>
+            </div>
+        `;
+        productGrid.innerHTML += markup;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderProducts('weed'); // Default category to show on page load
+
+    document.querySelectorAll('.sidebar a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const category = e.target.getAttribute('data-category');
+            renderProducts(category);
+        });
+    });
+});
